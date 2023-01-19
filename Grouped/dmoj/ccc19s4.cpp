417aef188bf2666338f416c93edf217048bb3528
Douglas <dcvweb123@gmail.com>
2023-01-19 10:12:37 -0500

    clean

--- Grouped/dmoj/ccc19s4.cpp
+++ Grouped/dmoj/ccc19s4.cpp
@@ -7,19 +7,13 @@ typedef long long ll;
 #define ms(a,x) memset(a,x,sizeof(a))
 #define SZ(x) int(x.size())
 
-<<<<<<< HEAD
 struct Node {
 	ll x=0, y=0, sm=0;
 };
 
 const int TSZ=1<<20;
-ll ar[TSZ], dp[TSZ];
+ll ar[TSZ], dp[TSZ], lazy[TSZ*2];
 Node tree[TSZ*2];
-ll lazy[TSZ*2];
-
-void printNode(Node a) {
-	// cout << "(" << a.x << "," << a.xy << ")";
-}
 
 void printTree() {
 	int i=1;
@@ -30,16 +24,7 @@ void printTree() {
 		}
 		cout << endl;
 	}
-	// for (int i=0; i<TSZ; i++) cout << "(" << tree[i].x << "," << tree[i].y << "," << tree[i].sm << ")" << " ";  cout << "\n";
-	// for (int i=TSZ; i<TSZ*2; i++) cout << "(" << tree[i].x << "," << tree[i].y << "," << tree[i].sm << ")" << " ";  cout << "\n";
 	cout << endl;
-}
-
-void printLazy() {
-	// cout << "lazy: ";
-	// for (int i=0; i<TSZ; i++) // cout << lazy[i] << " "; // cout << "| ";
-	// for (int i=TSZ; i<TSZ*2; i++) // cout << lazy[i] << " "; // cout << "\n";
-	// cout << endl;
 }
 
 Node merge(Node a, Node b, int y) {
@@ -47,7 +32,6 @@ Node merge(Node a, Node b, int y) {
 	res.sm=max({a.sm, b.sm});
 	return res;
 }
-
 
 void down(int node) {
 	int left=node*2, right=node*2+1;
@@ -101,15 +85,10 @@ Node query(int node, int segL, int segR, int l, int r) {
 	int mid=segL+segR>>1;
 	return merge(query(node*2, segL, mid, l, r), query(node*2+1, mid, segR, l, r), tree[node].y);
 }
-=======
-const int MAXN=1e6;
-ll ar[MAXN];
->>>>>>> bf4a286ae018ece9f90054e7bfc406e9865d112d
 
 int main() {
 	cin.sync_with_stdio(0);
 	cin.tie(0);
-<<<<<<< HEAD
 	
 	ms(ar,0);
 	ms(dp,0);
@@ -120,63 +99,10 @@ int main() {
 	for (int i=1; i<=N; i++) cin>>ar[i];
 	
 	for (int i=1; i<=N; i++) {
-		uptY(1,0,TSZ,0,i,ar[i]);
-
-		// cout << "set [" << 0 << "," << i << ").y->" << ar[i] << endl;
-		// printTree();
-
 		int L=max(0,i-K), R=(i-1)/K*K;
+		uptY(1,0,TSZ,0,i,ar[i]);
 		dp[i]=query(1,0,TSZ,L,R+1).sm;
-		// cout << "query [" << L << "," << R+1 << "): " << dp[i] << endl;
-		// printTree();
-		
 		uptX(1,0,TSZ,i,dp[i]);
-
-		// cout << "set [" << i << "].x->" << dp[i] << endl;
-		// printTree();
-
-		// printTree();
-		// // cout << "\t";
-		// printLazy();
 	}
-
-	// for (int i=1; i<=N; i++) cout << ar[i] << ' '; cout << endl;
-	// for (int i=1; i<=N; i++) cout << dp[i] << ' '; cout << endl;
 	cout << dp[N] << endl;
 }
-
-/*
-input:
-7 4
-6 4 8 9 3 2 4 
-
-wanted output:
-6 6 8 9 17 17 17
-17
-
-input: 
-6 3
-4 6 6 4 3 1 
-
-wanted output:
-10
-
-*/
-=======
-
-	int N,K; cin>>N>>K;
-	for (int i=0; i<N; i++) cin>>ar[i];
-	
-	ll ans=0;
-	stack<ll> a,b;
-	for (int i=N-1; i>=0; i--) b.push(i==N-1 ? ar[i] : max(b.top(), ar[i]));
-	for (int i=0; i<N; i++) {
-		a.push(i==0 ? ar[i] : max(a.top(), ar[i])), b.pop();
-		if (!SZ(a) || !SZ(b)) continue;
-		if (i+1<=K && N-i-1<=K) ans=max(ans,a.top()+b.top());
-	}
-	if (K==N) ans=*max_element(ar,ar+N);
-	cout << ans << endl;
-
-}
->>>>>>> bf4a286ae018ece9f90054e7bfc406e9865d112d
