75d72a710e644a648fcaa1f0b6cfdbb32dac9100
Douglas <dcvweb123@gmail.com>
2023-01-19 10:05:39 -0500

    finally solved ccc19s4

---
+++ Grouped/dmoj/mccc1s3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define ms(a,x) memset(a,x,sizeof(a))
+#define SZ(x) x.size()
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    string S;
+}
